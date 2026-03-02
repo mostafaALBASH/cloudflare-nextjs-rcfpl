2e902e864f7b2861cc2a0702a13d571137699152
mostafaALBASH <mostafa_elbesh@outlook.com>
2026-03-02 19:40:29 +0100

    Add Google Analytics tracking (G-Q7DWFQPHEW)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -109,6 +109,25 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         />
         {/* End Google Tag Manager */}
 
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-Q7DWFQPHEW"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-Q7DWFQPHEW');
+            `,
+          }}
+        />
+        {/* End Google Analytics */}
+
         {/* JSON-LD Structured Data */}
         <Script
           id="structured-data-webapp"
